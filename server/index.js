f8c159a5fd08c165a99add967592fa0a4dfd72a6
Theresa Stocking <nottodayjose@MacBook-Pro.local>
2019-03-29 14:13:10 -0500

    Step 2 Done

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+// const users = require("./users");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3060, () => console.log("Listening on 3060"));
